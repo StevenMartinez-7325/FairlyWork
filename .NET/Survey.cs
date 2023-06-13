0049fdfb8333117c1358ec7b4afdc98140f61b10
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-13 14:43:44 -0700

    Update Survey.cs

--- .NET/Survey.cs
+++ .NET/Survey.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.Surveys
+namespace Models.Domain.Surveys
 {
     public class Survey : BaseSurvey
     {
