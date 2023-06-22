7d223884dd74542ee60688a0fc03064369c2c518
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-22 10:20:05 -0700

    Update Survey.cs

--- .NET/Survey.cs
+++ .NET/Survey.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models.Domain.Surveys
 {
     public class Survey : BaseSurvey
     {
