e29d8c09b2e8969c273619ca0fa064e63c71286a
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-22 11:37:44 -0700

    Update BaseSurvey.cs

--- .NET/BaseSurvey.cs
+++ .NET/BaseSurvey.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models.Domain.Surveys
+public class BaseSurvey
 {
-    public class BaseSurvey
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string Name { get; set; }    
-    }
+    public string Name { get; set; }    
 }
+
