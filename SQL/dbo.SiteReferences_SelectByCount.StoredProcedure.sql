3017c27b20f7219fa471441624a8e75725926835
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-22 11:39:40 -0700

    Update dbo.SiteReferences_SelectByCount.StoredProcedure.sql

--- SQL/dbo.SiteReferences_SelectByCount.StoredProcedure.sql
+++ SQL/dbo.SiteReferences_SelectByCount.StoredProcedure.sql
@@ -5,24 +5,24 @@ as
 
 /*---------TEST CODE----------
 
-		Execute [dbo].[SiteReferences_SelectByCount] 
+	Execute [dbo].[SiteReferences_SelectByCount] 
 
 */
 
 BEGIN 
 
-		SELECT	sr.[ReferenceTypeId]
-				,rt.[Name]
-				,TotalCount = COUNT(sr.ReferenceTypeId) 
-		FROM [dbo].[ReferenceTypes] as rt
-					INNER JOIN
-				[dbo].[SiteReferences] as sr
-		ON sr.ReferenceTypeId = rt.Id
-					INNER JOIN 
-					[dbo].[Users] as u
-		ON u.Id = sr.UserId
-		GROUP BY sr.ReferenceTypeId, rt.Name
-		ORDER BY sr.ReferenceTypeId
+	SELECT	sr.[ReferenceTypeId]
+			,rt.[Name]
+			,TotalCount = COUNT(sr.ReferenceTypeId) 
+	FROM [dbo].[ReferenceTypes] as rt
+				INNER JOIN
+			[dbo].[SiteReferences] as sr
+	ON sr.ReferenceTypeId = rt.Id
+				INNER JOIN 
+				[dbo].[Users] as u
+	ON u.Id = sr.UserId
+	GROUP BY sr.ReferenceTypeId, rt.Name
+	ORDER BY sr.ReferenceTypeId
 
 
 
