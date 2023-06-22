7ba79858615dabebd15fbd0742b1badf096846a8
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-22 14:45:29 -0700

    Update dbo.Surveys_SelectAll_Paginated.StoredProcedure.sql

--- SQL/dbo.Surveys_SelectAll_Paginated.StoredProcedure.sql
+++ SQL/dbo.Surveys_SelectAll_Paginated.StoredProcedure.sql
@@ -5,47 +5,47 @@ as
 
 /*
 
-		Declare @PageIndex int = 0
-				,@PageSize int = 10
-
-		Execute [dbo].[Surveys_SelectAll_Paginated] 
-									@PageIndex 
-									,@PageSize
+	Declare @PageIndex int = 0
+			,@PageSize int = 10
+	
+	Execute [dbo].[Surveys_SelectAll_Paginated] 
+					@PageIndex 
+					,@PageSize
 
 */
 
 BEGIN 
 
-		Declare @offSet int = @PageIndex * @PageSize
-
-		SELECT s.[Id]
-			  ,s.[Name]
-			  ,[Description]
-			  ,ss.[Id]
-			  ,ss.[Name]							
-			  ,st.[Id]
-			  ,st.[Name]
-			  ,[CreatedBy]
-			  ,u.[FirstName]
-			  ,u.[LastName]
-			  ,u.[Mi]
-			  ,u.[AvatarUrl]
-			  ,s.[DateCreated]
-			  ,s.[DateModified]
-			  , TotalCount = COUNT(1) OVER() 
-		  FROM [dbo].[Surveys] as s inner join [dbo].[Users] as u
-						ON s.CreatedBy = u.Id
-						INNER JOIN
-						[dbo].[SurveyStatus] as ss
-						ON s.StatusId = ss.Id
-						INNER JOIN 
-						[dbo].[SurveyTypes] as st
-						ON s.SurveyTypeId = st.Id
-			
-		ORDER BY s.Id
-
-		OFFSET @offSet Rows
-		Fetch Next @PageSize Rows ONLY
+	Declare @offSet int = @PageIndex * @PageSize
+	
+	SELECT s.[Id]
+		  ,s.[Name]
+		  ,[Description]
+		  ,ss.[Id]
+		  ,ss.[Name]							
+		  ,st.[Id]
+		  ,st.[Name]
+		  ,[CreatedBy]
+		  ,u.[FirstName]
+		  ,u.[LastName]
+		  ,u.[Mi]
+		  ,u.[AvatarUrl]
+		  ,s.[DateCreated]
+		  ,s.[DateModified]
+		  , TotalCount = COUNT(1) OVER() 
+	  FROM [dbo].[Surveys] as s inner join [dbo].[Users] as u
+					ON s.CreatedBy = u.Id
+					INNER JOIN
+					[dbo].[SurveyStatus] as ss
+					ON s.StatusId = ss.Id
+					INNER JOIN 
+					[dbo].[SurveyTypes] as st
+					ON s.SurveyTypeId = st.Id
+		
+	ORDER BY s.Id
+	
+	OFFSET @offSet Rows
+	Fetch Next @PageSize Rows ONLY
 
 END 
 GO
