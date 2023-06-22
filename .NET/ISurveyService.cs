b272977d890f40c31555c5183e0da98aa616f95f
Steven Martinez <122942103+StevenMartinez-7325@users.noreply.github.com>
2023-06-22 11:38:02 -0700

    Update ISurveyService.cs

--- .NET/ISurveyService.cs
+++ .NET/ISurveyService.cs
@@ -1,15 +1,9 @@
-﻿using Models;
-using Models.Requests.Surveys;
-
-namespace Services.Interfaces.Survey
+public interface ISurveyService
 {
-    public interface ISurveyService
-    {
-        int AddSurvey(SurveyAddRequest model, int userId);
-        void DeleteSurvey(int id);
-        Paged<Models.Domain.Surveys.Survey> GetAllByPagination(int pageIndex, int pageSize);
-        Paged<Models.Domain.Surveys.Survey> GetByCreatedBy(int pageIndex, int pageSize, int userId);
-        Models.Domain.Surveys.Survey GetById(int id);
-        void UpdateSurvey(SurveyUpdateRequest request);
-    }
+    int AddSurvey(SurveyAddRequest model, int userId);
+    void DeleteSurvey(int id);
+    Paged<Models.Domain.Surveys.Survey> GetAllByPagination(int pageIndex, int pageSize);
+    Paged<Models.Domain.Surveys.Survey> GetByCreatedBy(int pageIndex, int pageSize, int userId);
+    Models.Domain.Surveys.Survey GetById(int id);
+    void UpdateSurvey(SurveyUpdateRequest request);
 }
